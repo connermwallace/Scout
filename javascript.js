aa2639a3da0e9864d81ee59ef166437ada286775
connermwallace <connerwallace@gmail.com>
2019-06-03 16:55:11 -0700

    "Added more features b/c extended deadline"

--- javascript.js
+++ javascript.js
@@ -101,9 +101,6 @@ function getMap() {
 function makeList() {
     var data = state.statedata;
     let listDiv = document.querySelector('#list');
-    let arttitle = document.createElement('h1');
-    arttitle.textContent = "Adventure Near You"
-    listDiv.appendChild(arttitle)
     // Different categories have different title for the fields we want, we need different
     // for loops to build the cards for all the categories.
     if (state.inputtedType == "parks") {
@@ -114,9 +111,10 @@ function makeList() {
             var name = data[i].name;
             let newName = document.createElement('a');
             newName.setAttribute('name', name);
-            newName.textContent = name;
-
+            let titlename = document.createElement('h3');
+            titlename.textContent = name + " Park";
             main.appendChild(newName);
+            main.appendChild(titlename);
 
             let lineBreak = document.createElement('br');
             main.appendChild(lineBreak);
@@ -217,49 +215,7 @@ function makeList() {
             listDiv.appendChild(main);
         }
     }
-    if (state.inputtedType == "parks") {
-        for (let i = 0; i < data.length; i++) {
-            let main = document.createElement('div');
-            main.classList.add("listCard");
-
-            var name = data[i].name;
-            let newName = document.createElement('a');
-            newName.setAttribute('name', name);
-            newName.textContent = name;
-
-            main.appendChild(newName);
-
-            let lineBreak = document.createElement('br');
-            main.appendChild(lineBreak);
-
-            var parkurl = data[i].url;
-            let newparkurl = document.createElement('a');
-            newparkurl.setAttribute("href", parkurl);
-            newparkurl.textContent = "Park Website";
-            main.appendChild(newparkurl);
-
-            var desc = data[i].description;
-            let newDesc = document.createElement('p');
-            newDesc.textContent = "Description: " + desc;
-            main.appendChild(newDesc);
-
-            var direct = data[i].directionsInfo;
-            let newdirect = document.createElement('p');
-            newdirect.textContent = "Directions: " + direct;
-            main.appendChild(newdirect);
-
-            var weather = data[i].weatherInfo;
-            let newweather = document.createElement('p');
-            newweather.textContent = "Weather Information: " + weather;
-            main.appendChild(newweather);
-
-            var desig = data[i].designation;
-            let newdesig = document.createElement('p');
-            newdesig.textContent = "Designation: " + desig;
-            main.appendChild(newdesig);
-            listDiv.appendChild(main);
-        }
-    }
+    
     if (state.inputtedType == "visitorcenters") {
         for (let i = 0; i < data.length; i++) {
             let main = document.createElement('div');
@@ -297,19 +253,21 @@ function makeList() {
 // Creates the cards for the articles section
 function makeArticles(newarticles) {
     let listDiv = document.querySelector('#articles');
-    let arttitle = document.createElement('h1');
-    arttitle.textContent = "Articles"
-    listDiv.appendChild(arttitle)
+        
     for (let i = 0; i < newarticles.length; i++) {
 
         let main = document.createElement('div');
         main.classList.add("listCard");
 
+        // var title = newarticles[i].title;
+        // let url = newarticles[i].url;
+        // let newName = document.createElement('a');
+        // newName.setAttribute('href', url)
+        // newName.textContent = title.substring(0, 30) + "..."
+        // main.appendChild(newName);
         var title = newarticles[i].title;
-        let url = newarticles[i].url;
-        let newName = document.createElement('a');
-        newName.setAttribute('href', url)
-        newName.textContent = title.substring(0, 30) + "..."
+        let newName = document.createElement('h3');
+        newName.textContent = title.substring(0, 35) + "..."
         main.appendChild(newName);
 
         let lineBreak = document.createElement('br');
@@ -326,7 +284,7 @@ function makeArticles(newarticles) {
 
         var desc = newarticles[i].listingdescription;
         let newDesc = document.createElement('p');
-        newDesc.textContent = desc.substring(0, 30) + "...";
+        newDesc.textContent = desc.substring(0, 50) + "...";
         main.appendChild(newDesc);
 
         listDiv.appendChild(main);
